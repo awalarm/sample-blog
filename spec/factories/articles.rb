6455c22476f7328b108e5115fd3cf23b60b50384
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-13 20:59:27 +0400

    update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,16 @@ FactoryGirl.define do
   factory :article do
     title "Article title"
     text "Article text"
+
+    # создаем фабрику с названеие artticle_with_comments
+    # для создания статьи с нескролькими комментариями
+
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        create_list :comment, 3, article: article
+
+      end    
+    end
+
   end
 end
